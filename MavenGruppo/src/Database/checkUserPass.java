29cd07da6968270d0e989b08c8b6a125f91b4306
Blg-John <Blg-John@Blg.homenet.telecomitalia.it>
2020-07-10 12:41:10 +0200

    aa

--- MavenGruppo/src/Database/checkUserPass.java
+++ MavenGruppo/src/Database/checkUserPass.java
@@ -5,16 +5,26 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+
+
 public class checkUserPass {
 
 	
 	public static boolean insertUser(String username, String password, String email) {
+		
 		try {
+			
+			 
+			String hashed = BCrypt.hashpw(password, BCrypt.gensalt(12));
+		    System.out.println(hashed);
+		      
 			Connection conn = DbConnect.getInstance().getConnection();
 			String query = "insert into webaccount values (?,?,?)";
 			PreparedStatement psm = conn.prepareStatement(query);
 			psm.setString(1, username);
-			psm.setString(2, password);
+			psm.setString(2, hashed);
 			psm.setString(3, email);
 			int i = psm.executeUpdate();
 			if(i>0) {
@@ -30,21 +40,45 @@ public class checkUserPass {
 	}
 	
 	
-	public static boolean login(String user, String pass) {
+	public static String returnPass(String user) {
+		
+	String query = "select password from webaccount where username=?";
+	String pass = "";
+		
 		try {
-			
 			Connection conn = DbConnect.getInstance().getConnection();
-			String query = "select * from webaccount where username =? and password =?";
 			PreparedStatement psm = conn.prepareStatement(query);
 			psm.setString(1, user);
-			psm.setString(2, pass);
 			ResultSet rs = psm.executeQuery();
 			if(rs != null && rs.next()) {
-				System.out.println("The user and pass are ok..");
-				return true;
-			}else {
-				System.out.println("Auth is not good");
+				pass = rs.getString("password");
+				return pass;
 			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		return pass;
+	}
+		
+		
+	public static boolean login(String user, String pass) {
+		
+		try {
+			
+		String passDbHash =	checkUserPass.returnPass(user);
+		
+		if(BCrypt.checkpw(pass, passDbHash) == true) {
+			System.out.println("Merge!!!--");
+			return true;
+		
+		}else {
+			System.out.println("NUUU!!!--");
+			return false;
+		}
+
 		}catch(Exception ex){
 			System.out.println("Unable to load Driver Class");
 		}
@@ -53,7 +87,7 @@ public class checkUserPass {
 	
 	public static boolean userExist(String user) {
 		
-		String query = "select username from login";
+		String query = "select username from webaccount";
 		
 		try {
 			Connection conn = DbConnect.getInstance().getConnection();
